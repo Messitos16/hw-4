71ca0ace113c013b99eebb8bfb13967996a1a421
Messitos16 <149245706+Messitos16@users.noreply.github.com>
2023-11-26 19:36:25 +0300

    Add files via upload

---
+++ 4/script.js
@@ -0,0 +1,74 @@
+const Mylife = {
+name: 'Dior',
+age:16,
+skills:['html', 'css', 'know 5-6 languages', 'play football very well' ],
+address:{
+    location: 'asia',
+    city: 'Osh',
+    country: 'Kyrgysztan',
+    
+    street:{
+        name: 'Djim',
+        number: 174
+    },
+hobbies:{
+   sports:'football'
+   
+},
+
+classmates:{
+    name: ['Nurseit', 'Bael', 'Kolya', 'Berador'],
+    age: [18, 15, 15, 15],
+    
+},
+exschool: {
+name: 'Gramotey',
+address: 'makmudova 28',
+director:'Akbaralieva Larisa Ruslanovna'
+},
+family: {
+    quantity: '7',
+    men:4,
+    women:3
+},
+LanguagesIKnow:{
+    languages:['Russian','Kyrgyz','English','Turkish',]
+},
+GamesILike:{
+    games:['Efootball','Fifa','Mortal Kombat']
+},
+FoodsILike:{
+    foods:['Pizza','Burger','Pasta']
+},
+LanguagesIStuding:{
+languages:['Arabic','Spanish']
+},
+MyFootballIdol:{
+    footballIdol:['Lionel Andres Messi','Xavi Ernandez','Andres Inesta','Tiago']
+},
+StyleILike:{
+style:['Old-money','Star-boy','Streat-wear']
+},
+CarsILike:{
+    cars:['Mercedes-Benz','Tesla','Lixiang']
+},
+TeamISupport:{
+    team:['Fc Barcelona','Fc Liverpool']
+},
+Fragnanses:{
+    frangans:['Tom-Ford','Armani Stronger With You']
+},
+FootIplay:{
+    foot:['Right','Left',]
+},
+CountriesIWantToVisit:{
+    countries:['Mekka','Medina','USA','Netherlands','Barcelona']
+},
+NationalTeamISupport:{
+    nationalTeam:['Argentina','Spain']
+}
+}    
+
+    };
+    
+    console.log(Mylife); 
